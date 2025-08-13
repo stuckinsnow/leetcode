8381fcdcff441739a24bc91dae26aeda6a8cded1
Philip <stuckinsnow@users.noreply.github.com>
2025-08-13 14:42:09 +0100

    feat: add valid anagram solutions in Go, JavaScript, and Python

---
+++ ValidAnagram/solution.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	sRunes, tRunes := []rune(s), []rune(t)
+	slices.Sort(sRunes)
+	slices.Sort(tRunes)
+
+	for i := range sRunes {
+		if sRunes[i] != tRunes[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	s := "anagram"
+	t := "nagaram"
+	fmt.Println(isAnagram(s, t))
+}
--- ValidAnagram/solution.py
+++ ValidAnagram/solution.py
@@ -1,3 +1,11 @@
-
 s = "anagram"
 t = "nagaram"
+
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    sorted_s = ''.join(sorted(s))
+    sorted_t = ''.join(sorted(t))
+    return sorted_s == sorted_t
+
+print(is_anagram(s, t))
